2d459a3cac87614efa75403b3bddaf3aa30655db
Boris Faure <boris@fau.re>
2024-02-10 21:11:32 +0100

    ui: represent status with beans

--- ronde_lib/src/html.rs
+++ ronde_lib/src/html.rs
@@ -1,5 +1,6 @@
-use crate::history::{CommandHistory, History};
+use crate::history::{CommandHistory, CommandHistoryEntry, History, TimeTag};
 use crate::summary::Summary;
+use chrono::{DateTime, Utc};
 use maud::{html, Markup, PreEscaped, Render, DOCTYPE};
 
 /// Render a header
@@ -8,6 +9,7 @@ fn header(summary: &Summary) -> Markup {
     html! {
         (DOCTYPE)
         meta charset="utf-8";
+        meta name="viewport" content="width=device-width, initial-scale=1";
         style { (PreEscaped(include_str!("style.css"))) }
         title {
             (format!("{} {}/{}",
@@ -16,11 +18,55 @@ fn header(summary: &Summary) -> Markup {
     }
 }
 
+impl Render for TimeTag {
+    fn render(&self) -> Markup {
+        match self {
+            TimeTag::Minute(m) => html! { (format!("{:02}",m)) },
+            TimeTag::Hour(h) => html! { (format!("{:02}",h)) },
+            TimeTag::Day(_) => html! { "D" },
+        }
+    }
+}
+
+/// Summary of a command history entry
+struct CommandHistoryEntrySummary {
+    timestamp: DateTime<Utc>,
+    on_error: bool,
+    tag: TimeTag,
+}
+impl CommandHistoryEntrySummary {
+    fn from_entry(entry: &CommandHistoryEntry) -> Self {
+        CommandHistoryEntrySummary {
+            timestamp: entry.timestamp,
+            on_error: entry.result.is_err(),
+            tag: entry.tag.clone(),
+        }
+    }
+}
+impl Render for CommandHistoryEntrySummary {
+    fn render(&self) -> Markup {
+        let klass = if self.on_error { "bean err" } else { "bean ok" };
+        let title = self.timestamp.to_rfc2822();
+        html! {
+            div class=(klass) title=(title) {
+                (self.tag)
+            }
+        }
+    }
+}
+
 /// Render a CommandResult
 impl Render for CommandHistory {
     fn render(&self) -> Markup {
         html! {
-            (self.name)
+            div class="command" {
+                h2 { (self.name) }
+                div class="bar" {
+                    @for entry in self.entries.iter() {
+                        (CommandHistoryEntrySummary::from_entry(&entry))
+                    }
+                }
+            }
         }
         /*
         match &self.result {
@@ -52,14 +98,14 @@ impl Render for Summary {
         if self.nb_err == 0 {
             html! {
                 h1 class="ok" {
-                    (format!("{} commands succeeded", self.nb_ok))
+                    "✔ All Systems Operational"
                 }
             }
         } else {
             let plural = if self.nb_err == 1 { "" } else { "s" };
             html! {
                 h1 class="err" {
-                    (format!("{} command{} failed", self.nb_err, plural))
+                    (format!("⚠ {} command{} failed ⚠", self.nb_err, plural))
                 }
             }
         }
