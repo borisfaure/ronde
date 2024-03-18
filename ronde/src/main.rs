c0eae62b6353cb357694ebb859a135f9a6244838
Boris Faure <boris@fau.re>
2024-03-18 21:55:29 +0100

    stop using clap for a single parameter
    Closes #14

--- ronde/src/main.rs
+++ ronde/src/main.rs
@@ -1,4 +1,3 @@
-use clap::{Arg, Command as ClapCommand};
 use futures::future::join_all;
 
 use ronde_lib::config::Config;
@@ -8,32 +7,36 @@ use ronde_lib::notification::check_and_send_notifications;
 use ronde_lib::runner;
 use ronde_lib::summary::Summary;
 
-/// Build a Command
-fn build_cli() -> ClapCommand {
-    ClapCommand::new("ronde")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Boris Faure <boris.faure@gmail.com>")
-        .about("Monitor your servers and services with alerting and a simple status page")
-        .arg(
-            Arg::new("ConfigFile")
-                .value_name("YamlConfigFile")
-                .num_args(1)
-                .short('c')
-                .long("config")
-                .required(true)
-                .help("YAML Config file describing the services to monitor"),
-        )
+/// Display usage
+fn usage() {
+    println!("ronde version {}", env!("CARGO_PKG_VERSION"));
+    println!("Monitor your servers and services with alerting and a simple status page");
+    println!();
+    println!("USAGE:");
+    println!("    ronde <YamlConfigFile>");
+    println!();
+    println!("FLAGS:");
+    println!("    -h, --help       Prints help information");
+    println!();
+    println!("ARGS:");
+    println!("    <YamlConfigFile>    YAML Config file describing the services to monitor");
 }
 
 #[tokio::main]
 /// Main function
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = build_cli().get_matches();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 || args[1] == "-h" || args[1] == "--help" {
+        usage();
+        return Ok(());
+    }
 
-    let yaml_file = matches.get_one::<String>("ConfigFile").unwrap();
+    let yaml_file = &args[1];
     let config = Config::load(yaml_file).await?;
 
     let results = join_all(config.commands.into_iter().map(runner::execute_command)).await;
+
+    /* TODO: stop running as root */
 
     let mut history = History::load(&config.history_file).await?;
 
